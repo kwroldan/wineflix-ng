4a35d37320ee06119aca9c10cc1879842d3904dc
Kristofer <kroldan@ford.com>
2022-05-17 12:32:22 -0400

    Refactor hero content

--- src/app/hero-content-description/hero-content-description.component.ts
+++ src/app/hero-content-description/hero-content-description.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { heroContent } from '../../data'
 
 @Component({
   selector: 'app-hero-content-description',
   templateUrl: './hero-content-description.component.html',
   styleUrls: ['./hero-content-description.component.css']
 })
-export class HeroContentDescriptionComponent {}
+export class HeroContentDescriptionComponent {
+  heroContent = heroContent;
+}
